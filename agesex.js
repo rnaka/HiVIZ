2912167ce2e0918e0c053e6c70f8791bb69c70b3
Derrick Luyen <46693824+derrickluyen@users.noreply.github.com>
2020-11-29 18:11:34 -1000

    update layout and content age and sex page, added two more pages (process and about us)

--- agesex.js
+++ agesex.js
@@ -1,233 +1,329 @@
-Plotly.d3.csv("Age_and_Sex_2019_Male.csv", function(data_male) {
-  Plotly.d3.csv("Age_and_Sex_2019_Female.csv", function (data_female) {
+// START OF MALE HAWAII TABLE
+var male_hi = [
+    ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+'],
+    ['86671', '83988', '101298', '99296', '84745', '86703', '84646', '54615', '25893']
+]
 
-    var cg_male = [];
-    var age_male = [];
-    var cg_male_new = [];
-    var age_male_new = [];
+var male_hi_data = [{
+  type: 'table',
+  header: {
+    values: [["<b>Age Group</b>"], ["<b>Population</b>"]],
+    align: "center",
+    line: { width: 1, color: 'black' },
+    fill: { color: "#004e7c" },
+    font: { family: "Arial", size: 12, color: "white" }
+  },
+  cells: {
+    values: male_hi,
+    align: "center",
+    line: { color: "black", width: 1 },
+    font: { family: "Arial", size: 11, color: ["black"] },
+    format: [
+      "",
+      "⠀>7,f",
+    ],
+  }
+}];
 
-    var cg_female = [];
-    var age_female = [];
-    var cg_female_new = [];
-    var age_female_new = [];
+var male_hi_table_layout = {
+  title: "Males in Hawaii by Age Group",
+  paper_bgcolor: "#dce1e3",
+  font: {
+    family: 'Arial',
+    size: 12,
+    color: '#333333'
+  },
+};
 
-    for (let i = 0; i < data_male.length; i++) {
-      cg_male.push(data_male[i].Category);
-      age_male.push(data_male[i].Population);
+Plotly.newPlot('table_male_hi', male_hi_data, male_hi_table_layout);
+// END OF MALE HAWAII TABLE
+
+// START OF MALE USA TABLE
+var male_usa = [
+  ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+'],
+  ['19972455', '21890966', '22832289', '22173510', '20108242', '20516060', '18242281', '10897682', '4955488']
+]
+
+var male_usa_data = [{
+  type: 'table',
+  header: {
+    values: [["<b>Age Group</b>"], ["<b>Population</b>"]],
+    align: "center",
+    line: { width: 1, color: 'black' },
+    fill: { color: "#b73225" },
+    font: { family: "Arial", size: 12, color: "white" }
+  },
+  cells: {
+    values: male_usa,
+    align: "center",
+    line: { color: "black", width: 1 },
+    font: { family: "Arial", size: 11, color: ["black"] },
+    format: [
+      "",
+      "⠀>10,f",
+    ],
+  }
+}];
+
+var male_usa_table_layout = {
+  title: "Males in USA by Age Group",
+  paper_bgcolor: "#dce1e3",
+  font: {
+    family: 'Arial',
+    size: 12,
+    color: '#333333'
+  },
+};
+
+Plotly.newPlot('table_male_usa', male_usa_data, male_usa_table_layout);
+// END OF MALE USA TABLE
+
+// START OF FEMALE HAWAII TABLE
+var female_hi = [
+  ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+'],
+  ['83130', '76315', '88182', '97517', '82205', '85656', '91237', '63132', '40643']
+]
+
+var female_hi_data = [{
+  type: 'table',
+  header: {
+    values: [["<b>Age Group</b>"], ["<b>Population</b>"]],
+    align: "center",
+    line: { width: 1, color: 'black' },
+    fill: { color: "#004e7c" },
+    font: { family: "Arial", size: 12, color: "white" }
+  },
+  cells: {
+    values: female_hi,
+    align: "center",
+    line: { color: "black", width: 1 },
+    font: { family: "Arial", size: 11, color: ["black"] },
+    format: [
+      "",
+      "⠀>7,f",
+    ],
+  }
+}];
+
+var female_hi_table_layout = {
+  title: "Females in Hawaii by Age Group",
+  paper_bgcolor: "#dce1e3",
+  font: {
+    family: 'Arial',
+    size: 12,
+    color: '#333333'
+  },
+};
+
+Plotly.newPlot('table_female_hi', female_hi_data, female_hi_table_layout);
+// END OF FEMALE HAWAII TABLE
+
+// START OF FEMALE USA TABLE
+var female_usa = [
+  ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+'],
+  ['19122817', '20886037', '21869690', '21899925', '20476570', '21432881', '20168785', '13010630', '7783215']
+]
+
+var female_usa_data = [{
+  type: 'table',
+  header: {
+    values: [["<b>Age Group</b>"], ["<b>Population</b>"]],
+    align: "center",
+    line: { width: 1, color: 'black' },
+    fill: { color: "#b73225" },
+    font: { family: "Arial", size: 12, color: "white" }
+  },
+  cells: {
+    values: female_usa,
+    align: "center",
+    line: { color: "black", width: 1 },
+    font: { family: "Arial", size: 11, color: ["black"] },
+    format: [
+      "",
+      "⠀>10,f",
+    ],
+  }
+}];
+
+var female_usa_table_layout = {
+  title: "Females in USA by Age Group",
+  paper_bgcolor: "#dce1e3",
+  font: {
+    family: 'Arial',
+    size: 12,
+    color: '#333333'
+  },
+};
+
+Plotly.newPlot('table_female_usa', female_usa_data, female_usa_table_layout);
+// END OF FEMALE USA TABLE
+
+// START OF HAWAII VS US MALE BAR GRAPH
+var barMalePlotLayout = {
+  title: "Male Age Groups: Hawaii vs USA (2019)",
+  barmode: "group",
+  paper_bgcolor: "#dce1e3",
+  font: {
+    family: 'Arial',
+    size: 12,
+    color: '#333333'
+  },
+  xaxis: {
+    automargin: true,
+    title: {
+      text: "Age Group",
     }
-
-    cg_male_new.push("0-9");
-    cg_male_new.push("10-19");
-    cg_male_new.push("20-29");
-    cg_male_new.push("30-39");
-    cg_male_new.push("40-49");
-    cg_male_new.push("50-59");
-    cg_male_new.push("60-69");
-    cg_male_new.push("70-79");
-    cg_male_new.push("80+");
-
-    age_male_new.push((parseInt(age_male[1]) + parseInt(age_male[2])).toString());
-    age_male_new.push((parseInt(age_male[3]) + parseInt(age_male[4]) + parseInt(age_male[5])).toString());
-    age_male_new.push((parseInt(age_male[6]) + parseInt(age_male[7]) + parseInt(age_male[8]) + parseInt(age_male[9])).toString());
-    age_male_new.push((parseInt(age_male[10]) + parseInt(age_male[11])).toString());
-    age_male_new.push((parseInt(age_male[12]) + parseInt(age_male[13])).toString());
-    age_male_new.push((parseInt(age_male[14]) + parseInt(age_male[15])).toString());
-    age_male_new.push((parseInt(age_male[16]) + parseInt(age_male[17]) + parseInt(age_male[18]) + parseInt(age_male[19])).toString());
-    age_male_new.push((parseInt(age_male[20]) + parseInt(age_male[21])).toString());
-    age_male_new.push((parseInt(age_male[22]) + parseInt(age_male[23])).toString());
-
-    for (let i = 0; i < data_female.length; i++) {
-      cg_female.push(data_female[i].Category);
-      age_female.push(data_female[i].Population);
+  },
+  yaxis: {
+    automargin: true,
+    title: {
+      text: "Population (%)",
     }
+  },
+};
 
-    cg_female_new.push("0-9");
-    cg_female_new.push("10-19");
-    cg_female_new.push("20-29");
-    cg_female_new.push("30-39");
-    cg_female_new.push("40-49");
-    cg_female_new.push("50-59");
-    cg_female_new.push("60-69");
-    cg_female_new.push("70-79");
-    cg_female_new.push("80+");
+// START FIRST TRACE
+var trace_male_hi_bar = {
+  mode: "markers",
+  x: male_hi[0],
+  y: [12.244, 11.865, 14.311, 14.028, 11.972, 12.249, 11.958, 7.716, 3.658],
+  type: 'bar',
+  name: 'HI',
+  marker: {
+    color: '#004e7c',
+  },
+  hovertemplate: "HI: %{y}%",
+};
+// END FIRST TRACE
 
-    age_female_new.push((parseInt(age_female[1]) + parseInt(age_female[2])).toString());
-    age_female_new.push((parseInt(age_female[3]) + parseInt(age_female[4]) + parseInt(age_female[5])).toString());
-    age_female_new.push((parseInt(age_female[6]) + parseInt(age_female[7]) + parseInt(age_female[8]) + parseInt(age_female[9])).toString());
-    age_female_new.push((parseInt(age_female[10]) + parseInt(age_female[11])).toString());
-    age_female_new.push((parseInt(age_female[12]) + parseInt(age_female[13])).toString());
-    age_female_new.push((parseInt(age_female[14]) + parseInt(age_female[15])).toString());
-    age_female_new.push((parseInt(age_female[16]) + parseInt(age_female[17]) + parseInt(age_female[18]) + parseInt(age_female[19])).toString());
-    age_female_new.push((parseInt(age_female[20]) + parseInt(age_female[21])).toString());
-    age_female_new.push((parseInt(age_female[22]) + parseInt(age_female[23])).toString());
+// START SECOND TRACE
+var trace_male_usa_bar = {
+  mode: "markers",
+  x: male_usa[0],
+  y: [12.360, 13.547, 14.130, 13.722, 12.444, 12.696, 11.289, 6.744, 3.067],
+  type: 'bar',
+  name: 'USA',
+  marker: {
+    color: '#b73225',
+  },
+  hovertemplate: "USA: %{y}%",
+};
 
-    // get male table div
-    tableDivMale = document.getElementById('table_male');
+var trace_male_bar_data = [trace_male_hi_bar, trace_male_usa_bar];
 
-    // get female table div
-    tableDivFemale = document.getElementById('table_female');
+Plotly.newPlot('hi_usa_male_bar', trace_male_bar_data, barMalePlotLayout);
+// END OF HAWAII VS US MALE BAR GRAPH
 
-    // START MALE TABLE
-    // based off of example: https://plotly.com/javascript/table/
-    var tableDataMale = [{
-      type: 'table',
-      header: {
-        values: [["<b>Age Group</b>"], ["<b>Population</b>"]],
-        align: "center",
-        line: { width: 1, color: 'black' },
-        fill: { color: "#004e7c" },
-        font: { family: "Arial", size: 12, color: "white" }
-      },
-      cells: {
-        values: [cg_male_new, age_male_new],
-        align: "center",
-        line: { color: "black", width: 1 },
-        font: { family: "Arial", size: 11, color: ["black"] },
-        format: [
-          "",
-          "⠀>7,f",
-        ],
-      }
-    }];
+// START OF HAWAII VS US FEMALE BAR GRAPH
+var barFemalePlotLayout = {
+  title: "Female Age Groups: Hawaii vs USA (2019)",
+  barmode: "group",
+  paper_bgcolor: "#dce1e3",
+  font: {
+    family: 'Arial',
+    size: 12,
+    color: '#333333'
+  },
+  xaxis: {
+    automargin: true,
+    title: {
+      text: "Age Group",
+    }
+  },
+  yaxis: {
+    automargin: true,
+    title: {
+      text: "Population (%)",
+    }
+  },
+};
 
-    var tableLayoutMale = {
-      title: "Males in Hawaii by Age Group",
-      paper_bgcolor: "#dce1e3",
-      font: {
-        family: 'Arial',
-        size: 12,
-        color: '#333333'
-      },
-    };
+// START FIRST TRACE
+var trace_female_hi_bar = {
+  mode: "markers",
+  x: female_hi[0],
+  y: [11.741, 10.779, 12.455, 13.773, 11.611, 12.098, 12.886, 8.917, 5.740],
+  type: 'bar',
+  name: 'HI',
+  marker: {
+    color: '#004e7c',
+  },
+  hovertemplate: "HI: %{y}%",
+};
+// END FIRST TRACE
 
-    var myTable = Plotly.plot(tableDivMale, tableDataMale, tableLayoutMale);
-    // END MALE TABLE
+// START SECOND TRACE
+var trace_female_usa_bar = {
+  mode: "markers",
+  x: female_usa[0],
+  y: [11.475, 12.533, 13.123, 13.141, 12.287, 12.861, 12.102, 7.807, 4.670],
+  type: 'bar',
+  name: 'USA',
+  marker: {
+    color: '#b73225',
+  },
+  hovertemplate: "USA: %{y}%",
+};
 
-    // START FEMALE TABLE
-    // based off of example: https://plotly.com/javascript/table/
-    var tableDataFemale = [{
-      type: 'table',
-      header: {
-        values: [["<b>Age Group</b>"], ["<b>Population</b>"]],
-        align: "center",
-        line: { width: 1, color: 'black' },
-        fill: { color: "#b73225" },
-        font: { family: "Arial", size: 12, color: "white" },
-      },
-      cells: {
-        values: [cg_female_new, age_female_new],
-        align: "center",
-        line: { color: "black", width: 1 },
-        font: { family: "Arial", size: 11, color: ["black"] },
-        format: [
-          "",
-          " >4,3f",
-        ],
-      }
-    }];
+var trace_female_bar_data = [trace_female_hi_bar, trace_female_usa_bar];
 
-    var tableLayoutFemale = {
-      title: "Females in Hawaii by Age Group",
-      paper_bgcolor: "#dce1e3",
-      font: {
-        family: 'Arial',
-        size: 12,
-        color: '#333333'
-      },
-    };
+Plotly.newPlot('hi_usa_female_bar', trace_female_bar_data, barFemalePlotLayout);
+// END OF HAWAII VS US FEMALE BAR GRAPH
 
-    var myTable = Plotly.plot(tableDivFemale, tableDataFemale, tableLayoutFemale);
-    // END FEMALE TABLE
+// START HAWAII PIE
+var plotLayoutHiPie = {
+  title: "Male vs Female Ratio in Hawaii",
+  font: {
+    family: 'Arial',
+    size: 12,
+    color: '#333333'
+  },
+  showlegend: false,
+  paper_bgcolor: "#dce1e3",
+  piecolorway: ["#b73225", "#004e7c"],
+  height: 440
+};
 
-    asplotdiv = document.getElementById("as_plot");
+var hiPie = {
+  labels: ['Male', 'Female'],
+  values: [707855, 708017],
+  type: 'pie',
+  mode: 'markers',
+  texttemplate: "<b>%{label}</b><br>%{value}<br>%{percent}",
+  rotation: 30,
+  name: 'Male vs Female Ratio in Hawaii'
+};
 
-    // set plot layout
-    var plotLayout = {
-      title: "Age Groups in Hawaii (2019)",
-      paper_bgcolor: "#dce1e3",
-      font: {
-        family: 'Arial',
-        size: 12,
-        color: '#333333'
-      },
-      xaxis: {
-        automargin: true,
-        title: {
-          text: "Age Group",
-        }
-      },
-      yaxis: {
-        automargin: true,
-        title: {
-          text: "Population",
-        }
-      },
-    };
+data_hi_pie = [hiPie];
 
-    // START FIRST TRACE
-    var trace1 = {
-      mode: "markers",
-      x: cg_male_new,
-      y: age_male_new,
-      type: 'bar',
-      name: 'Male',
-      marker: {
-        color: '#004e7c',
-      }
-    };
+Plotly.newPlot('hi_pie', data_hi_pie, plotLayoutHiPie);
+// END HAWAII PIE
 
-    data1 = [trace1];
+// START USA PIE
+var plotLayoutUsaPie = {
+  title: "Male vs Female Ratio in USA",
+  font: {
+    family: 'Arial',
+    size: 12,
+    color: '#333333'
+  },
+  showlegend: false,
+  paper_bgcolor: "#dce1e3",
+  piecolorway: ["#b73225", "#004e7c"],
+  height: 440
+};
 
-    var myChart = Plotly.plot(asplotdiv, data1, plotLayout);
-    // END FIRST TRACE
+var usaPie = {
+  labels: ['Male', 'Female'],
+  values: [161588973, 166650550],
+  type: 'pie',
+  mode: 'markers',
+  texttemplate: "<b>%{label}</b><br>%{value}<br>%{percent}",
+  rotation: 30,
+  name: 'Male vs Female Ratio in USA'
+};
 
-    // START SECOND TRACE
-    var trace2 = {
-      mode: "markers",
-      x: cg_female_new,
-      y: age_female_new,
-      type: 'bar',
-      name: 'Female',
-      marker: {
-        color: '#b73225',
-      }
-    };
+data_usa_pie = [usaPie];
 
-    data2 = [trace2];
-
-    var myChart2 = Plotly.plot(asplotdiv, data2, plotLayout);
-    // END SECOND TRACE
-
-    // pie chart
-    var plotLayoutPie = {
-      title: "Male vs Female Ratio in Hawaii",
-      font: {
-        family: 'Arial',
-        size: 12,
-        color: '#333333'
-      },
-      showlegend: false,
-      paper_bgcolor: "#dce1e3",
-      piecolorway: ["#b73225", "#004e7c"],
-      height: 440
-    };
-
-    pieDiv = document.getElementById("as_pie");
-
-    // START THIRD TRACE
-    var trace = {
-      labels: ['Male', 'Female'],
-      values: [age_male[0], age_female[0]],
-      type: 'pie',
-      mode: 'markers',
-      texttemplate: "<b>%{label}</b><br>%{value}<br>%{percent}",
-      rotation: 30,
-      name: 'Male vs Female Ratio in Hawaii'
-    };
-
-    data = [trace];
-
-    var myChart = Plotly.plot(pieDiv, data, plotLayoutPie);
-
-  });
-});
+Plotly.newPlot('usa_pie', data_usa_pie, plotLayoutUsaPie);
+// END USA PIE
